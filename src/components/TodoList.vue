b426c201f5ac7d8374bb1ff6c23216c729d8cd47
ToD <wlunareve@gmail.com>
2021-08-21 17:12:46 +0800

    fix: use id instead of index to control list, avoid list length changed from side effect

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -41,12 +41,12 @@
     </div>
 
     <div
-      v-for="(todo, index) in todoList" 
-      :key="index" 
+      v-for="(todo) in todoList" 
+      :key="todo.ID" 
       class="todo__item flex items-center"
     >
       <input 
-        :id="index" 
+        :id="todo.ID" 
         v-model="todo.status"
         class="todo__checkbox h-6 w-6 rounded bg-gray-200 border-transparent 
           focus:border-transparent focus:bg-green-200 text-green-700 
@@ -59,19 +59,19 @@
         :class="[
           todo.status ? finishClass : '',
         ]"
-        :for="index"
+        :for="todo.ID"
       >
         {{ todo.content }}
       </label>
       <button 
         class="btn"
-        @click="toggleModifyTodo(index)"
+        @click="toggleModifyTodo(todo.ID)"
       >
         Edit
       </button>
       <button 
         class="btn"
-        @click="deleteTodo(index)"
+        @click="deleteTodo(todo.ID)"
       >
         Delete
       </button>
@@ -80,33 +80,45 @@
 </template>
 
 <script>
-import { ref, toRef, toRefs, computed } from 'vue'
+import { ref, computed } from 'vue'
 
 export default {
   setup() {
     const todoList = ref([
       {
+        ID: 0,
         status: false,
         content: 'Work'
       },
       {
+        ID: 1,
         status: true,
         content: 'Exercise'
       },
     ])
     
     const newTodoContent = ref('')
-    const indexPrepareToModify = ref(-1)
-    const isEditStatus = computed(() => indexPrepareToModify.value >= 0)
-
+    const idPrepareToModify = ref('')
     const finishClass = 'line-through text-gray-400'
+
+    const isEditStatus = computed(() => idPrepareToModify.value !== '')
+
+    const makeIdGenerator = () => {
+      let latestTodoId = todoList.value.slice(-1)[0].ID
+      return () =>  {
+        latestTodoId += 1
+        return latestTodoId
+      }
+    }
+    const generateTodoId = makeIdGenerator()
     
     const addTodo = () => {
       if (!newTodoContent.value) return alert('Need todo content')
-    
+      
       todoList.value = [
         ...todoList.value,
         {
+          ID: generateTodoId(),
           status: false,
           content: newTodoContent.value
         }
@@ -114,15 +126,15 @@ export default {
       newTodoContent.value = ''
     }
     
-    const toggleModifyTodo = (indexToModify) => {
-      indexPrepareToModify.value = indexToModify
-      newTodoContent.value = todoList.value.find((todo, index) => index === indexToModify).content
+    const toggleModifyTodo = (idToModify) => {
+      idPrepareToModify.value = idToModify
+      newTodoContent.value = todoList.value.find(({ ID }) => ID === idToModify).content
     }
     
     const modifyTodo = () => {
       if (isEditStatus.value) {
-        const newTodoList = todoList.value.map((todo, index) => {
-          if (index === indexPrepareToModify.value) {
+        const newTodoList = todoList.value.map((todo) => {
+          if (todo.ID === idPrepareToModify.value) {
             // 研究解構賦的 ref 會怎麼樣
             return {
               ...todo,
@@ -134,17 +146,17 @@ export default {
         })
         todoList.value = newTodoList
         newTodoContent.value = ''
-        indexPrepareToModify.value = -1
+        idPrepareToModify.value = ''
       }
     }
 
     const cancelModifyTodo = () => {
-      indexPrepareToModify.value = -1
+      idPrepareToModify.value = ''
       newTodoContent.value = ''
     }
     
-    const deleteTodo = (indexToDelete) => {
-      todoList.value = todoList.value.filter((todo, index) => index !== indexToDelete)
+    const deleteTodo = (idToDelete) => {
+      todoList.value = todoList.value.filter(({ ID }) => ID !== idToDelete)
     }
 
     return {
@@ -167,6 +179,7 @@ export default {
 
 .btn {
   min-width: 5rem;
+
   @apply bg-green-500 hover:bg-green-700 text-white font-bold mx-2 py-2 px-4 rounded-xl;
 }
 </style>
